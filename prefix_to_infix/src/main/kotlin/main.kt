1a8593c32043b05ab2d4d0f53664277530485ab0
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-10-09 11:19:47 +0800

    Create throws update main function

--- prefix_to_infix/src/main/kotlin/main.kt
+++ prefix_to_infix/src/main/kotlin/main.kt
@@ -1,12 +1,13 @@
+
 fun check_operator(elem: String): Boolean {
     return elem in arrayListOf<String>("+", "*", "-", "/")
 }
 
 fun check_operand(elem: String): Boolean {
-    return elem.toIntOrNull() != null
+    return elem.toIntOrNull() != null && elem.toInt() >= 0
 }
 
-fun to_infix(elems: String): Any {
+fun to_infix(elems: String): String {
     val elems = elems.split(" ")
     val stack = arrayListOf<String>()
     for (elem in elems.reversed()){
@@ -16,7 +17,7 @@ fun to_infix(elems: String): Any {
         else if (check_operator(elem)) {
 
             if (stack.size < 2) {
-                return "Invalid expression"
+                throw IllegalArgumentException("Invalid expression")
             }
             val operand1 = stack.last()
             stack.remove(stack.last())
@@ -25,24 +26,30 @@ fun to_infix(elems: String): Any {
             val expression = "($operand1 $elem $operand2)"
             stack.add(expression)
         }else {
-            return "Invalid token"
+            throw IllegalArgumentException("Invalid element")
         }
     }
     if (stack.size != 1) {
-        return "Invalid expression"
+        throw IllegalArgumentException("Invalid expression")
     }
     return stack.first()
 }
 
 
-fun main(){
+fun main() : Int{
     val prefix_notations = arrayListOf<String>(
-            "+ - 13 4 55",
-            "+ 2 * 2 - 2 1",
-            "+ + 10 20 30",
-            "- - 1 2",
-            "/ + 3 10 * + 2 3 - 3 5")
+        "+ - 13 4 55",
+        "+ 2 * 2 - 2 1",
+        "+ + 10 20 30",
+        "- - 1 2",
+        "/ + 3 10 * + 2 3 - 3 5")
     for (notation in prefix_notations){
-        println(to_infix(notation))
+        try {
+            println(notation + ": " + to_infix(notation))
+        }
+        catch(message: IllegalArgumentException){
+            println("$message")
+        }
     }
+    return 0
 }
