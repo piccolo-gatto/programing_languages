a5cf4bc39f7b33a06bafae1a6fb0e61a44a60ae3
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-11-19 22:12:42 +0800

    Fix class type

--- pascalInterpreter/src/main/kotlin/interpreter/Ast.kt
+++ pascalInterpreter/src/main/kotlin/interpreter/Ast.kt
@@ -3,10 +3,10 @@ package interpreter
 abstract class Node(){
 }
 
-class Number(val token: Token) : Node()
-class BinOp(val left: Node, val op: Token, val right: Node) : Node()
-class UnarOp(val op: Token, val value: Node) : Node()
-class Empty(val value: String = "") : Node()
-class Variable(val token: Token, val value: Node) : Node()
-class Identifier(val token: Token) : Node()
-class Semicolon(val left: Node, val right: Node) : Node()
+data class Number(val token: Token) : Node()
+data class BinOp(val left: Node, val op: Token, val right: Node) : Node()
+data class UnarOp(val op: Token, val value: Node) : Node()
+data class Empty(val value: String = "") : Node()
+data class Variable(val token: Token, val value: Node) : Node()
+data class Identifier(val token: Token) : Node()
+data class Semicolon(val left: Node, val right: Node) : Node()
